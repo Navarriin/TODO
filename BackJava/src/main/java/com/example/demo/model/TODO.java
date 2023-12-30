4129bbb9bde6d3b8f70ccfb4669e2cc8a65865c9
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-30 19:57:06 -0300

    configurando ambiente BackEnd

---
+++ BackJava/src/main/java/com/example/demo/model/TODO.java
@@ -0,0 +1,55 @@
+package com.example.demo.model;
+
+import com.example.demo.enums.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class TODO {
+
+    @Id
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    @Size(max = 300)
+    @Column(length = 300, nullable = false)
+    private String content;
+
+    @NotNull
+    @NotBlank
+    @Column(nullable = false)
+    private Status status = Status.ACTIVE;
+
+    public TODO() {}
+
+    public TODO(Long id, String content, Status status) {
+        this.id = id;
+        this.content = content;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
