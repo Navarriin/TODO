e6852166ee7a551fbb2a0cebb23708e1a9b9dfb9
Gabriel <gabrielnavarro.vip@gmail.com>
2024-01-02 19:59:52 -0300

    criando Mapper

---
+++ BackJava/src/main/java/com/example/demo/dto/mapper/TODOMapper.java
@@ -0,0 +1,27 @@
+package com.example.demo.dto.mapper;
+
+import com.example.demo.dto.TODODTO;
+import com.example.demo.model.TODO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TODOMapper {
+    public static TODODTO toDto(TODO todo) {
+        if(todo == null) {
+            return null;
+        }
+        return new TODODTO(todo.getId(), todo.getContent(), todo.getStatus());
+    }
+
+    public TODO toEntity( TODODTO todoDto) {
+        if(todoDto == null) {
+            return null;
+        }
+        TODO todo = new TODO();
+        if(todoDto.id() != null) {
+            todo.setId(todoDto.id());
+        }
+        todo.setContent(todoDto.content());
+        return todo;
+    }
+}
